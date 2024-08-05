0c0265d2f76a55aa61558a192552237b24493197
Lilla Kavecsanszki <lilla-kavecsanszki@outlook.com>
2024-08-05 22:23:08 +0200

    Added contact page

--- residenza126/settings.py
+++ residenza126/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'residenza',
     'properties',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
--- residenza126/urls.py
+++ residenza126/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('residenza.urls')),
+    path('contact/', include('contact.urls')),
 ]
